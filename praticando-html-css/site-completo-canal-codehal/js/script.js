e8b95028f35e515bf95d458faee7cf16425e713c
jonas-souza <126021388+jonas-souza@users.noreply.github.com>
2023-04-04 13:49:08 -0400

    atualizacao

--- praticando-html-css/site-completo-canal-codehal/js/script.js
+++ praticando-html-css/site-completo-canal-codehal/js/script.js
@@ -34,3 +34,15 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+
+/* ================================== scroll reveal =================================*/
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contact form', { origin: 'bottom' });
