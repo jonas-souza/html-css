75be10b61eb831c5021e19a35c84e073f22ee44a
jonas-souza <126021388+jonas-souza@users.noreply.github.com>
2023-04-04 13:52:50 -0400

    update .js

--- praticando-html-css/site-completo-canal-codehal/js/script.js
+++ praticando-html-css/site-completo-canal-codehal/js/script.js
@@ -46,3 +46,5 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
